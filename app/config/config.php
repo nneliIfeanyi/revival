b1b89f2d5928c8dd3d253d5e988b04ea77d54804
Stanvic <stanvicbest@gmail.com>
2024-11-26 03:08:22 +0100

    going live

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php
 // DB Params
 define("DB_HOST", "localhost");
-define("DB_USER", "revivallabourers");
+define("DB_USER", "stanvicc_revivallabourers");
 define("DB_PASS", "revivallabourers");
 define("DB_NAME", "stanvicc_revival");
 
