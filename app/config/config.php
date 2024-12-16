8172692f8a408812cbb317fb74f0c9d38b6b5014
Stanvic <stanvicbest@gmail.com>
2024-12-16 14:59:26 +0100

    core site config

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,13 @@
 <?php
 // DB Params
 define("DB_HOST", "localhost");
-define("DB_USER", "stanvicc_revivallabourers");
-define("DB_PASS", "revivallabourers");
-define("DB_NAME", "stanvicc_revival");
+define("DB_USER", "revivall_labourer");
+define("DB_PASS", "revival@2025");
+define("DB_NAME", "revivall_labourers");
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'https://sample.stanvic.com.ng');
+define('URLROOT', 'https://revivallabourers.org.ng');
 // Site Name
 define('SITENAME', 'Revival Labourers');
