c913bfa9d1ae07f2d6ee14aff87b673d51fb7c2e
Stanvic <stanvicbest@gmail.com>
2024-12-16 15:25:49 +0100

    redirect

--- app/config/config.php
+++ app/config/config.php
@@ -1,5 +1,17 @@
 <?php
 // DB Params
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
+// define("DB_NAME", "revival");
+
+// // App Root
+// define('APPROOT', dirname(dirname(__FILE__)));
+// // URL Root
+// define('URLROOT', 'http://localhost/revival');
+// // Site Name
+// define('SITENAME', 'Revival Labourers');
+
 define("DB_HOST", "localhost");
 define("DB_USER", "revivall_labourer");
 define("DB_PASS", "revival@2025");
@@ -8,6 +20,6 @@ define("DB_NAME", "revivall_labourers");
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'https://revivallabourers.org.ng');
+define('URLROOT', 'https://home.revivallabourers.org.ng');
 // Site Name
 define('SITENAME', 'Revival Labourers');
