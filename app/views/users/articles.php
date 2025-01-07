6ce50d5f6d47349596a2bb1fa8f10af605c683d7
Stanvic <stanvicbest@gmail.com>
2025-01-07 13:59:21 +0100

    changes

--- app/views/users/articles.php
+++ app/views/users/articles.php
@@ -23,8 +23,8 @@
             <input id="title" type="text" name="title" required class="form-control form-control-lg" value="<?php echo $data['title']; ?>">
           </div>
           <div class="form-group">
-            <label for="content">Content <span style="font-size: x-small;">(use new lines for paragraphs)</span></label>
-            <textarea id="content" autocomplete="on" name="content" placeholder="Write here" cols="50" rows="15" class="form-control"><?php echo $data['content']; ?></textarea>
+            <label for="content">Content</label>
+            <textarea id="content" name="content" placeholder="Write here" cols="50" rows="15" class="form-control"><?php echo $data['content']; ?></textarea>
           </div>
 
           <div class="form-group">
@@ -141,87 +141,23 @@
 <?php require APPROOT . '/views/inc/foot.php'; ?>
 <script src="<?= URLROOT; ?>/tinymce/tinymce.min.js"></script>
 <script>
-  /**
-   * Initiate TinyMCE Editor
-   */
-
-  const useDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const isSmallScreen = window.matchMedia('(max-width: 1023.5px)').matches;
-
   tinymce.init({
     selector: 'textarea',
-    plugins: 'preview importcss searchreplace autolink autosave save visualblocks visualchars fullscreen link table charmap pagebreak nonbreaking insertdatetime advlist lists wordcount charmap quickbars emoticons accordion',
-    editimage_cors_hosts: ['picsum.photos'],
-    menubar: 'edit view insert format tools table',
-    toolbar: "undo redo | blocks | bold italic underline strikethrough | align numlist bullist | link | table | lineheight outdent indent| forecolor backcolor removeformat | accordion accordionremove | charmap emoticons | fullscreen preview | print | pagebreak",
-    autosave_ask_before_unload: true,
-    autosave_interval: '30s',
-    autosave_prefix: '{path}{query}-{id}-',
-    autosave_restore_when_empty: false,
-    autosave_retention: '2m',
-    image_advtab: true,
-    link_list: [{
-        title: 'My page 1',
-        value: 'https://www.tiny.cloud'
-      },
-      {
-        title: 'My page 2',
-        value: 'http://www.moxiecode.com'
-      }
-    ],
-    image_list: [{
-        title: 'My page 1',
-        value: 'https://www.tiny.cloud'
-      },
-      {
-        title: 'My page 2',
-        value: 'http://www.moxiecode.com'
-      }
-    ],
-    image_class_list: [{
-        title: 'None',
-        value: ''
-      },
-      {
-        title: 'Some class',
-        value: 'class-name'
-      }
-    ],
-    importcss_append: true,
-    file_picker_callback: (callback, value, meta) => {
-      /* Provide file and text for the link dialog */
-      if (meta.filetype === 'file') {
-        callback('https://www.google.com/logos/google.jpg', {
-          text: 'My text'
-        });
-      }
-
-      /* Provide image and alt text for the image dialog */
-      if (meta.filetype === 'image') {
-        callback('https://www.google.com/logos/google.jpg', {
-          alt: 'My alt text'
-        });
-      }
-
-      /* Provide alternative source and posted for the media dialog */
-      if (meta.filetype === 'media') {
-        callback('http://localhost/revival/videos/video.mp4', {
-          source2: 'alt.ogg',
-          poster: 'https://www.google.com/logos/google.jpg'
-        });
-      }
-    },
-    height: 600,
-    image_caption: true,
-    quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
-    noneditable_class: 'mceNonEditable',
-    toolbar_mode: 'sliding',
-    contextmenu: 'link image table',
+    height: 210,
+    plugins: 'charmap emoticon wordcount table pagebreak codesample',
+    menubar: 'edit insert format tools',
+    toolbar: 'undo redo | superscript subscript bold underline strikethrough | lineheight outdent indent | charmap',
+    newline_behavior: 'linebreak',
     statusbar: false,
     promotion: false,
-    skin: useDarkMode ? 'oxide-dark' : 'oxide',
-    content_css: useDarkMode ? 'dark' : 'default',
-    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
+    setup: (editor) => {
+
+      editor.ui.registry.addButton('dash', {
+        text: '__________',
+        onAction: (_) => editor.insertContent(`__________`)
+      });
+
+    },
   });
 </script>
 </body>
