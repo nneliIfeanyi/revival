dcacd2a5e02a89a37d5d5f61a9c5a6a1004e7751
nneliifeanyi <stanvicbest@gmail.com>
2025-09-17 21:24:46 +0100

    config

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,25 @@
 <?php
 // DB Params
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
+// define("DB_NAME", "revival");
+
+// // App Root
+// define('APPROOT', dirname(dirname(__FILE__)));
+// // URL Root
+// define('URLROOT', 'http://localhost/revival');
+// // Site Name
+// define('SITENAME', 'Revival Labourers');
+
 define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "revivallabourers_DB");
+define("DB_USER", "revivall_labourer");
+define("DB_PASS", "revival@2025");
+define("DB_NAME", "revivall_labourers");
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'http://localhost/revival');
+define('URLROOT', 'https://home.revivallabourers.org.ng');
 // Site Name
 define('SITENAME', 'Revival Labourers');
